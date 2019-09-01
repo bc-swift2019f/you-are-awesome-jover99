b85add5f65911732dc20674ab2f2c9a375c96d25
Richard Jove <jover@bc.edu>
2019-09-01 13:52:32 -0400

    You Are Awesome completed

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Richard Jove on 8/25/19.
+//  Copyright © 2019 Richard Jove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
