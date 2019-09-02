e984d3d4b4218d969170114fe384c8718287f815
Richard Jove <jover@bc.edu>
2019-09-02 12:31:03 -0400

    2.6 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -18,15 +18,14 @@ class ViewController: UIViewController {
         //How do I make circle go away?
         
     }
-
+//Does the fact that color is red in utility pane do anything to the label? Or what residual effect does it have?
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        messageLabel.textColor = UIColor.red //Does the fact that color is red in utility pane do anything to the label? Or what residual effect does it have?
-    }
-        
-    @IBAction func showAnotherMessage(_ sender: UIButton) {
-        messageLabel.text = "You Are Great!"
-        messageLabel.textColor = UIColor.blue
+        if messageLabel.text == "You Are Awesome!"{
+            messageLabel.text = "You Are Great!"
+        } else if messageLabel.text == "You Are Great!" {
+            messageLabel.text = "You Are Amazing!"//This is case sensitive!
+        } else {
+            messageLabel.text = "You Are Awesome!"
     }
 }
-
+}
